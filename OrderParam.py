944df4ca64ca546d935f4795b977e6fd2708ecb4
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-02-01 19:23:35 +0530

    Add files via upload

--- OrderParam.py
+++ OrderParam.py
@@ -3,35 +3,36 @@ def OrderParam(strategy_name,signal):
     if strategy_name =='TREND_EMA':
         # directional option selling strategy when upside movement is expected
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-3,'transtype':'BUY','Qty':15}
-            p2 = {'opt':'PE', 'step':0,'transtype':'SELL','Qty':15}
+            p1 = {'opt':'CE', 'step':0,'transtype':'BUY','Qty':15}
+            p2 = {'opt':'CE', 'step':5,'transtype':'SELL','Qty':15}
             OrderPar = {'p1':p1, 'p2':p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 3, 'transtype': 'BUY', 'Qty': 15}
-            p2 = {'opt': 'CE', 'step': 0, 'transtype': 'SELL', 'Qty': 15}
+            p1 = {'opt': 'PE', 'step': 0, 'transtype': 'BUY', 'Qty': 15}
+            p2 = {'opt': 'PE', 'step':-5, 'transtype': 'SELL', 'Qty': 15}
             OrderPar = {'p1':p1, 'p2':p2}
 
     elif strategy_name == 'SharpeRev':
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-3,'transtype':'BUY','Qty':15}
-            p2 = {'opt':'PE', 'step':0,'transtype':'SELL','Qty':15}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'CE', 'step': 0, 'transtype': 'BUY', 'Qty': 15}
+            p2 = {'opt': 'CE', 'step': 5, 'transtype': 'SELL', 'Qty': 15}
+            OrderPar = {'p1': p1, 'p2': p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 3, 'transtype': 'BUY', 'Qty': 15}
-            p2 = {'opt': 'CE', 'step': 0, 'transtype': 'SELL', 'Qty': 15}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'PE', 'step': 0, 'transtype': 'BUY', 'Qty': 15}
+            p2 = {'opt': 'PE', 'step': -5, 'transtype': 'SELL', 'Qty': 15}
+            OrderPar = {'p1': p1, 'p2': p2}
+
 
     elif strategy_name == 'MOM_BURST':
         if signal == 1:
-            p1 = {'opt':'PE', 'step':-3,'transtype':'BUY','Qty':15}
-            p2 = {'opt':'PE', 'step':0,'transtype':'SELL','Qty':15}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'CE', 'step': 0, 'transtype': 'BUY', 'Qty': 15}
+            p2 = {'opt': 'CE', 'step': 5, 'transtype': 'SELL', 'Qty': 15}
+            OrderPar = {'p1': p1, 'p2': p2}
 
         elif signal == -1:
-            p1 = {'opt': 'CE', 'step': 3, 'transtype': 'BUY', 'Qty': 15}
-            p2 = {'opt': 'CE', 'step': 0, 'transtype': 'SELL', 'Qty': 15}
-            OrderPar = {'p1':p1, 'p2':p2}
+            p1 = {'opt': 'PE', 'step': 0, 'transtype': 'BUY', 'Qty': 15}
+            p2 = {'opt': 'PE', 'step': -5, 'transtype': 'SELL', 'Qty': 15}
+            OrderPar = {'p1': p1, 'p2': p2}
 
     return OrderPar
