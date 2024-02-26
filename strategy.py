c218afa417f8c5617d2bd6cac53ef5632006b186
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-02-26 09:06:24 +0530

    Add files via upload

--- strategy.py
+++ strategy.py
@@ -5,7 +5,7 @@ from OrderParam import OrderParam
 import schedule
 from datetime import datetime
 from strategy_repo import STRATEGY_REPO
-from database import GetOpenPosition
+from database import GetOpenPosition , get_expiry
 
 
 class StrategyFactory(STRATEGY_REPO):
@@ -37,14 +37,14 @@ class StrategyFactory(STRATEGY_REPO):
             valid_time = True
         return valid_time
 
-    def get_instrument(self, option_type, step):
+    def get_instrument(self, option_type, step, expiry_idx):
         # calculating option strike price
         interval = self.strike_interval[self.symbol]
         self.spot = self.LIVE_FEED.get_ltp(self.symbol) if not self.spot else self.spot
         strike = lambda: (round(self.spot / interval)) * interval
         ATM = strike()
         stk = ATM + interval * step
-        instrument = f'{self.index}{self.expiry}{option_type[0]}{stk}'
+        instrument = f'{self.index}{self.expiry[expiry_idx]}{option_type[0]}{stk}'
         # appending into the list for future use
         self.instrument_under_strategy.append(instrument)
 
@@ -54,7 +54,7 @@ class StrategyFactory(STRATEGY_REPO):
         if not self.instrument_under_strategy:
             self.param = {}
             for key, value in OrderParam(self.strategy_name, self.signal).items():
-                instrument = self.get_instrument(value['opt'], value['step'])
+                instrument = self.get_instrument(value['opt'], value['step'], value['expiry'])
                 self.param[instrument] = {'Instrument': instrument, 'Transtype': value['transtype'],
                                           'Qty': value['Qty'],'signal':self.signal}
 
@@ -85,6 +85,7 @@ class StrategyFactory(STRATEGY_REPO):
         if self.Is_Valid_time():
             if not self.overnight_flag:
                 self.Validate_OvernightPosition()
+                self.expiry = get_expiry(self.index)
                 if not self.scheduler.jobs:
                     self.scheduler.every(5).seconds.do(self.OrderManger.Update_OpenPosition)
             else:
