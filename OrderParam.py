79060410ea14464cf3c64a20b70177a371d945b9
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-04-05 09:55:07 +0530

    Add files via upload

--- OrderParam.py
+++ OrderParam.py
@@ -8,24 +8,24 @@ def OrderParam( Signals, index, is_expiry=False):
     p1 = p2 = spread = None
 
     if Signals > 0:
-        if is_expiry:
+        # if is_expiry:
             # credit spread
             p1 = {'opt': 'PE', 'step': -6, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'CREDIT'}
             p2 = {'opt': 'PE', 'step': -3, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0, 'spread':'CREDIT'}
-        else:
-            # debit spread
-            p1 = {'opt': 'CE', 'step': 0, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
-            p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
+        # else:
+        #     # debit spread
+        #     p1 = {'opt': 'CE', 'step': 0, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
+        #     p2 = {'opt': 'CE', 'step': 4, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
 
     elif Signals < 0:
-        if is_expiry:
+        # if is_expiry:
             # credit spread
             p1 = {'opt': 'CE', 'step': 6, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'CREDIT'}
             p2 = {'opt': 'CE', 'step': 3, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0,'spread':'CREDIT'}
-        else:
-            # debit spread
-            p1 = {'opt': 'PE', 'step': 0, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
-            p2 = {'opt': 'PE', 'step': -4, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0,'spread':'DEBIT'}
+        # else:
+        #     # debit spread
+        #     p1 = {'opt': 'PE', 'step': 0, 'transtype': 'BUY', 'Qty': QTY, 'expiry': 0, 'spread':'DEBIT'}
+        #     p2 = {'opt': 'PE', 'step': -4, 'transtype': 'SELL', 'Qty': QTY, 'expiry': 0,'spread':'DEBIT'}
 
     return {'p1': p1, 'p2': p2}
 
