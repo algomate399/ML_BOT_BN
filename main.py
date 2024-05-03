9908629f175da6ca0a8c1be9d6942de1ed13a4ef
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-05-03 18:53:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,7 @@ class TradingConsole:
                 long_bias = np.sum(long_proba)/np.sum(short_proba)
                 short_bias = np.sum(short_proba)/np.sum(long_proba)
                 AlgoTrader_GPT.bias = {'long':long_bias, 'short':short_bias}
+                print(AlgoTrader_GPT.bias , long_proba  , short_proba)
                 self.processed_flag = True
 
             for model in self.AlgoTrader:
