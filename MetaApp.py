90ce6bafae93aaba012b88f3f1e5f3f61ce91b13
algomate399 <155889177+algomate399@users.noreply.github.com>
2025-09-09 22:27:58 +0530

    Update MetaApp.py

--- MetaApp.py
+++ MetaApp.py
@@ -29,7 +29,7 @@ class MetaApi:
 
         # sending email response :
         msg='Engine refreshed @ :{}'.format(datetime.now(self.time_zone))
-        # self.send_email_notification(msg)
+        self.send_email_notification(msg)
 
     def Refresh_Var(self):
         self.error=None
@@ -108,4 +108,5 @@ class MetaApi:
 
         except Exception as e:
             self.error = f'Error:{e}'
+
             print(self.error)
