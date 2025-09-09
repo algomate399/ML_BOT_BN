60b5a47f4d13f7dd422f19cd41b2aa2b531dade2
algomate399 <155889177+algomate399@users.noreply.github.com>
2025-09-10 00:04:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def secondary_task():
         api.Refresh_Var()
         api.UpdateHistory()
         api.GenerateSignal()
-        print(api.Signals)
+        print('Signal' , api.Signals)
         sig_sum = np.sum([abs(s) for s in api.Signals.values()])
         if sig_sum:
             fx.RefreshVar()
@@ -73,6 +73,7 @@ def submit_signal():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
