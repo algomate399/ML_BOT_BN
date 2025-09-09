891aa28af6da029a9983c09b49f1b5ea75cfc565
algomate399 <155889177+algomate399@users.noreply.github.com>
2025-09-09 22:26:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import time
 from ClassLib import *
 from MetaApp import MetaApi
 from Forex_api import ForexApi
 import threading
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-from pytz import timezone
+from flask import Flask, request, render_template , jsonify
 import asyncio
-from flask import Flask, request, render_template
 import __main__
 
 # setting Attributes to Main
@@ -15,35 +11,25 @@ setattr(__main__ , 'NoiseEnhancer' ,NoiseEnhancer)
 setattr(__main__ , 'BaggingBootstrapper' ,BaggingBootstrapper)
 
 currency = ['EURUSD' ,'GBPUSD' , 'NZDUSD']
-metals = []
 
 api = MetaApi()
 fx = ForexApi()
 
-async def primary_task_1():
-    if connected and currency:
+async def primary_task():
         fx.RefreshVar()
         fx.symbol_list = currency
         fx.action='close_all_positions'
         await fx.start()
 
 
-async def primary_task_2():
-    if connected and metals:
-        fx.RefreshVar()
-        fx.symbol_list = metals
-        fx.action = 'close_all_positions'
-        await fx.start()
-
 async def secondary_task():
-    if connected:
         api.Refresh_Var()
         api.UpdateHistory()
         api.GenerateSignal()
         sig_sum = np.sum([abs(s) for s in api.Signals.values()])
         if sig_sum:
             fx.RefreshVar()
-            fx.symbol_list=metals+currency
+            fx.symbol_list=currency
             fx.action='execute_signals'
             fx.Signals = api.Signals
             fx.sl_in_pips = api.Sl_in_PiP
@@ -55,61 +41,33 @@ async def secondary_task():
             api.send_email_notification(api.Signals)
 
 
-app = Flask(__name__)
-connected = False
-passkey = '141990'
-scheduler = None
+def run_async(x):
+    asyncio.run(x)
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def Homepage():
-    title = 'Algomate-GPT(AI Driven Model)'
+    title = 'Algomate-GPT (AI Driven Model)'
     return render_template('index.html', title=title)
 
 
-@app.route('/on_connect', methods=['POST'])
-def On_connect():
-    global connected, scheduler
-    status = ''
-    json_data = request.get_json()
+@app.route('/close_all_positions')
+def process_signals():
 
-    if json_data['passkey'] == passkey:
-        if not connected:
-            connected = True
+    t = threading.Thread(target=run_async , args=(primary_task() , ))
+    t.start()
 
-            # Create and start the asyncio event loop
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+    return jsonify({"status": "ok"})
 
-            # Initialize the AsyncIOScheduler with the loop
-            scheduler = AsyncIOScheduler(event_loop=loop)
+@app.route('/submit_signals')
+def submit_signal():
 
-            # Add tasks to the scheduler
-            scheduler.add_job(primary_task_1, CronTrigger(hour=2, minute=15, timezone=timezone('Asia/Kolkata'), day_of_week='tue-sat'))
-            # scheduler.add_job(primary_task_2, CronTrigger(hour=3, minute=25, timezone=timezone('Asia/Kolkata'), day_of_week='tue-sat'))
-            scheduler.add_job(secondary_task, CronTrigger(hour=3, minute=35, timezone=timezone('Asia/Kolkata'), day_of_week='mon-fri'))
-            # Start the scheduler
-            scheduler.start()
+    t = threading.Thread(target=run_async , args=(secondary_task() , ))
+    t.start()
 
-            # Start the asyncio loop in a background thread
-            threading.Thread(target=loop.run_forever, daemon=True).start()
-
-            status = 'engine is running'
-        else:
-            status = 'engine stopped'
-            connected = False
-    else:
-        status = 'Incorrect Pass key'
-
-    return status
-
-
-@app.route('/get_connection_status')
-def get_connection_status():
-    if connected:
-        return 'engine is running'
-    else:
-        return 'not connected'
+    return jsonify({"status": "ok"})
 
 
 if __name__ == '__main__':
